9c1395b1df5d4883490a29a297c2b868132c071e
Olena Reukova <kronverk01@gmail.com>
2023-10-13 16:39:10 +0100

    @types/styled-components

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import Cards from './Cards';
 
-const data = [
-  'Radio App',
-  'E-commerce App',
-  'CYF progress tracker',
-  'Artificial Intelligence',
-  'Hotel booking website',
-  'TV project',
-  'Flowers shop',
-];
+// const data = [
+//   'Radio App',
+//   'E-commerce App',
+//   'CYF progress tracker',
+//   'Artificial Intelligence',
+//   'Hotel booking website',
+//   'TV project',
+//   'Flowers shop',
+// ];
 
 const Section = styled.div`
-  height: 100vh;
+  /* height: 100vh; */
   scroll-snap-align: center;
   display: flex;
   justify-content: center;
@@ -21,6 +22,7 @@ const Section = styled.div`
 const Container = styled.div`
   width: 1400px;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
 
   @media only screen and (max-width: 768px) {
@@ -31,80 +33,81 @@ const Container = styled.div`
   }
 `;
 
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+// const Left = styled.div`
+//   flex: 1;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
 
-  @media only screen and (max-width: 768px) {
-    padding: 20px;
-    justify-content: center;
-  }
-`;
+//   @media only screen and (max-width: 768px) {
+//     padding: 20px;
+//     justify-content: center;
+//   }
+// `;
 
-const List = styled.ul`
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
+// const List = styled.ul`
+//   list-style: none;
+//   display: flex;
+//   flex-direction: column;
+//   gap: 20px;
+// `;
 
-const ListItem = styled.li`
-  font-size: 60px;
-  font-weight: bold;
-  cursor: pointer;
-  color: white;
-  position: relative;
+// const ListItem = styled.li`
+//   font-size: 60px;
+//   font-weight: bold;
+//   cursor: pointer;
+//   color: white;
+//   position: relative;
 
-  @media only screen and (max-width: 768px) {
-    font-size: 24px;
-    color: white;
-    -webkit-text-stroke: 0px;
-  }
+//   @media only screen and (max-width: 768px) {
+//     font-size: 24px;
+//     color: white;
+//     -webkit-text-stroke: 0px;
+//   }
 
-  ::after {
-    content: '${(props) => props.text}';
-    position: absolute;
-    top: 0;
-    left: 0;
-    color: #f45050;
-    width: 0px;
-    overflow: hidden;
-    white-space: nowrap;
-  }
+//   ::after {
+//     content: '${(props) => props.text}';
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     color: #f45050;
+//     width: 0px;
+//     overflow: hidden;
+//     white-space: nowrap;
+//   }
 
-  &:hover {
-    ::after {
-      animation: moveText 0.5s linear both;
+//   &:hover {
+//     ::after {
+//       animation: moveText 0.5s linear both;
 
-      @keyframes moveText {
-        to {
-          width: 100%;
-        }
-      }
-    }
-  }
-`;
+//       @keyframes moveText {
+//         to {
+//           width: 100%;
+//         }
+//       }
+//     }
+//   }
+// `;
 
-const Right = styled.div`
-  flex: 1;
-`;
+// const Right = styled.div`
+//   flex: 1;
+// `;
 
 const Works = () => {
   return (
     <Section>
       <Container>
-        <Left>
-          <List>
+        {/* <Left> */}
+        <Cards />
+        {/* <List>
             {data.map((item) => (
               <ListItem key={item} text={item}>
                 {item}
               </ListItem>
             ))}
-          </List>
-        </Left>
-        <Right></Right>
+          </List> */}
+        {/* </Left> */}
+        {/* <Right></Right> */}
       </Container>
     </Section>
   );
