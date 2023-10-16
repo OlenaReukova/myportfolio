dd7fc1fe9c3b93268c11cafecef798aebaea7433
Olena Reukova <kronverk01@gmail.com>
2023-10-16 16:46:25 +0100

    Cleaning code in Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,34 +14,16 @@ const CardImage = styled.div`
   border-top-left-radius: 15px;
   border-top-right-radius: 15px;
   background-size: cover;
-  height: 410px; /* Adjust the height to your preference */
+  height: 410px;
 `;
 
 const CardTextWrapper = styled.div`
   padding: 20px;
 `;
 
-// const CardTextDate = styled.span`
-//   color: rgb(255, 7, 110);
-//   font-size: 13px;
-// `;
-
 const CardTextTitle = styled.h2`
   margin-top: 0px;
   font-size: 1.5rem;
-  /* background: linear-gradient(
-    110.78deg,
-    rgb(118, 230, 80) -1.13%,
-    rgb(249, 214, 73) 15.22%,
-    rgb(240, 142, 53) 32.09%,
-    rgb(236, 81, 87) 48.96%,
-    rgb(255, 24, 189) 67.94%,
-    rgb(26, 75, 255) 85.34%,
-    rgb(98, 216, 249) 99.57%
-  );
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent; */
 `;
 
 const CardTextBody = styled.p`
