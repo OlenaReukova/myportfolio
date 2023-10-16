08d48d63fd9a0ad101ba2ac9df8f99af46cf585f
Olena Reukova <kronverk01@gmail.com>
2023-10-16 16:47:27 +0100

    cleaning code in Works component

--- src/components/Works.js
+++ src/components/Works.js
@@ -2,18 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Cards from './Cards';
 
-// const data = [
-//   'Radio App',
-//   'E-commerce App',
-//   'CYF progress tracker',
-//   'Artificial Intelligence',
-//   'Hotel booking website',
-//   'TV project',
-//   'Flowers shop',
-// ];
-
 const Section = styled.div`
-  /* height: 100vh; */
   scroll-snap-align: center;
   display: flex;
   justify-content: center;
@@ -32,66 +21,6 @@ const Container = styled.div`
     justify-content: center;
   }
 `;
-
-// const Left = styled.div`
-//   flex: 1;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-
-//   @media only screen and (max-width: 768px) {
-//     padding: 20px;
-//     justify-content: center;
-//   }
-// `;
-
-// const List = styled.ul`
-//   list-style: none;
-//   display: flex;
-//   flex-direction: column;
-//   gap: 20px;
-// `;
-
-// const ListItem = styled.li`
-//   font-size: 60px;
-//   font-weight: bold;
-//   cursor: pointer;
-//   color: white;
-//   position: relative;
-
-//   @media only screen and (max-width: 768px) {
-//     font-size: 24px;
-//     color: white;
-//     -webkit-text-stroke: 0px;
-//   }
-
-//   ::after {
-//     content: '${(props) => props.text}';
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     color: #f45050;
-//     width: 0px;
-//     overflow: hidden;
-//     white-space: nowrap;
-//   }
-
-//   &:hover {
-//     ::after {
-//       animation: moveText 0.5s linear both;
-
-//       @keyframes moveText {
-//         to {
-//           width: 100%;
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// const Right = styled.div`
-//   flex: 1;
-// `;
 
 const Works = () => {
   return (
